d856f1021cbb5ac366e7a33e6bbd68ec9ba527e2
Marco Antonio <67771574+tonho991@users.noreply.github.com>
2023-11-22 17:28:45 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: 'Projetos Senac Alunos',
   description: 'A turma Front-End do SENAC (2023.29.6) é composta por alunos talentosos e apaixonados por tecnologia. Cada um deles possui habilidades únicas em programação, design, e desenvolvimento web, e estão sempre em busca de novos desafios para aprimorar seus conhecimentos. Ao longo do curso, os alunos desenvolveram projetos, com objetivos claros, metodologias bem estruturadas e resultados surpreendentes. Eles enfrentaram desafios, encontraram soluções criativas e aprenderam lições valiosas ao longo do processo.',
   openGraph: {
-    images: ["https://iili.io/JCGyYAJ.png"]
+    images: ["https://projetos-alunos-senac.vercel.app/alunos_photo.jpeg"]
   }
 }
 
